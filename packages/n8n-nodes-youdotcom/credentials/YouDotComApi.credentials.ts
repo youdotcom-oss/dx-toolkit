ad9ab340fc1b5067b253ad3001f6de3f879d8ba2
Edward Irby <EdwardIrby@users.noreply.github.com>
2026-02-04 16:04:19 -0800

    Correct url in YouDotComApi.credentials.ts

--- packages/n8n-nodes-youdotcom/credentials/YouDotComApi.credentials.ts
+++ packages/n8n-nodes-youdotcom/credentials/YouDotComApi.credentials.ts
@@ -22,7 +22,7 @@ export class YouDotComApi implements ICredentialType {
       default: '',
       required: true,
       description:
-        'Your You.com API key. Get one at <a href="https://you.com/platform" target="_blank">you.com/platform</a>',
+        'Your You.com API key. Get one at <a href="https://you.com/platform/api-keys" target="_blank">you.com/platform/api-keys</a>',
     },
   ]
 
